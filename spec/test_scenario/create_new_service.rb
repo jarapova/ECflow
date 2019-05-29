c0f48f6db2d7c0a863b1a53125c393b5aa60e01b
jarapova <arapova.jul2@gmail.com>
2019-05-29 10:28:16 +0300

    run pipeline

--- spec/test_scenario/create_new_service.rb
+++ spec/test_scenario/create_new_service.rb
@@ -6,59 +6,59 @@ feature 'TrueAutomation.IO capybara example' do
   scenario 'EC flow - create new env' do
     visit 'https://ectest.trueautomation.io/flow'
 
-# login
+    # login
     find(:xpath, "//input[@id='username']").set('admin')
     find(:xpath, "//input[@id='password']").set('changeme')
     find(:xpath, "//button[text()='Login']").click
-#
-    # find(ta(   'EC:picker:headerNav:serviceCatalogs')).click
-    # find(ta(   'EC:headerNav:serviceCatalogs')).click
+
+    # find(ta('EC:picker:headerNav:serviceCatalogs')).click
+    # find(ta('EC:headerNav:serviceCatalogs')).click
     find(:xpath, ta('EC:headerNav:serviceCatalogs', "//div[@class='icon-nav-item at-icon-nav-item-catalogs']")).click
     # find(:xpath, "//div[@class='icon-nav-item at-icon-nav-item-catalogs']").click
 
-    # find(ta(   'EC:picker:content:addNewCatalog')).click
-    # find(ta(   'EC:content:addNewCatalog')).click
+    # find(ta('EC:picker:content:addNewCatalog')).click
+    # find(ta('EC:content:addNewCatalog')).click
     find(:xpath, ta('EC:content:addNewCatalog', "//span[@class='sds-svg-icon__wrapper sds-svg-icon__wrapper--button-create-new at-new-catalog at-add-item-btn']")).click
     # find(:xpath, "//span[@class='sds-svg-icon__wrapper sds-svg-icon__wrapper--button-create-new at-new-catalog at-add-item-btn']").click
 
-    # find(ta(   'EC:picker:content:createNew')).click
-    # find(ta(   'EC:content:createNew')).click
+    # find(ta('EC:picker:content:createNew')).click
+    # find(ta('EC:content:createNew')).click
     find(:xpath, ta('EC:content:createNew', "(//div[@class='object-create__type'])[1]")).click
     # find(:xpath, "(//div[@class='object-create__type'])[1]").click
 
-    # find(ta(   'EC:picker:content:nameInput')).set('servCatalog'+SecureRandom.hex(4))
-    # find(ta(   'EC:content:nameInput')).set('servCatalog'+SecureRandom.hex(4))
+    # find(ta('EC:picker:content:nameInput')).set('servCatalog'+SecureRandom.hex(4))
+    # find(ta('EC:content:nameInput')).set('servCatalog'+SecureRandom.hex(4))
     find(:xpath, ta('EC:content:nameInput', "//input[@placeholder='Name']")).set('servCatalog'+SecureRandom.hex(4))
     # find(:xpath, "//input[@placeholder='Name']").set('servCatalog'+SecureRandom.hex(4))
 
-    # find(ta(   'EC:picker:content:selectProj')).click
-    # find(ta(   'EC:content:selectProj')).click
+    # find(ta('EC:picker:content:selectProj')).click
+    # find(ta('EC:content:selectProj')).click
     find(:xpath, ta('EC:content:selectProj', "//div[@class='at-select-header-title ec-select-header-title']")).click
     # find(:xpath, "//div[@class='at-select-header-title ec-select-header-title']").click
 
     sleep 3
-    # find(ta(   'EC:picker:content:defaultProj')).click
-    # find(ta(   'EC:content:defaultProj')).click
+    # find(ta('EC:picker:content:defaultProj')).click
+    # find(ta('EC:content:defaultProj')).click
     find(:xpath, ta('EC:content:defaultProj', "(//div[@title='Default'])[6]")).click
     # find(:xpath, "(//div[@title='Default'])[6]").click
 
-    # find(ta(   'EC:picker:content:okBtn')).click
-    # find(ta(   'EC:content:okBtn')).click
+    # find(ta('EC:picker:content:okBtn')).click
+    # find(ta('EC:content:okBtn')).click
     find(:xpath, ta('EC:content:okBtn', "//div[@class='ec-wizard-btn ec-ok at-ok-btn']")).click
     # find(:xpath, "//div[@class='ec-wizard-btn ec-ok at-ok-btn']").click
 
-    # find(ta(   'EC:picker:content:editName')).set('Ololo'+SecureRandom.hex(4))
-    # find(ta(   'EC:content:editName')).set('Ololo'+SecureRandom.hex(4))
+    # find(ta('EC:picker:content:editName')).set('Ololo'+SecureRandom.hex(4))
+    # find(ta('EC:content:editName')).set('Ololo'+SecureRandom.hex(4))
     find(:xpath, ta('EC:content:editName', "//input[@placeholder='Name']")).set('Ololo'+SecureRandom.hex(4))
     # find(:xpath, "//input[@placeholder='Name']").set('Ololo'+SecureRandom.hex(4))
 
-    # find(ta(   'EC:picker:content:descriptionCatalog')).set('text'+SecureRandom.hex(4))
-    # find(ta(   'EC:content:descriptionCatalog')).set('text'+SecureRandom.hex(4))
+    # find(ta('EC:picker:content:descriptionCatalog')).set('text'+SecureRandom.hex(4))
+    # find(ta('EC:content:descriptionCatalog')).set('text'+SecureRandom.hex(4))
     find(:xpath, ta('EC:content:descriptionCatalog', "//textarea[@placeholder='Description']")).set('text'+SecureRandom.hex(4))
     # find(:xpath, "//textarea[@placeholder='Description']").set('text'+SecureRandom.hex(4))
 
-    # find(ta(   'EC:picker:content:doneBtn')).click
-    # find(ta(   'EC:content:doneBtn')).click
+    # find(ta('EC:picker:content:doneBtn')).click
+    # find(ta('EC:content:doneBtn')).click
     find(:xpath, ta('EC:content:doneBtn', "//span[text()='Done']")).click
     # find(:xpath, "//span[text()='Done']").click
 
---
+++ spec/test_scenario/runPipeline.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+require 'securerandom'
+random_string = SecureRandom.hex
+
+feature 'TrueAutomation.IO capybara example' do
+  scenario 'EC flow - run pipeline with manual stage task' do
+    visit 'https://ectest.trueautomation.io/flow'
+
+# login
+    find(:xpath, "//input[@id='username']").set('admin')
+    find(:xpath, "//input[@id='password']").set('changeme')
+    find(:xpath, "//button[text()='Login']").click
+
+    sleep 3
+
+    find(ta('EC:picker:content:humburg')).click
+    # find(ta('EC:content:humburg')).click
+    # find(:xpath, ta('EC:content:humburg', "//div[@class='at-main-menu-btn ch-icon-container menu']")).click
+    # find(:xpath, "//div[@class='at-main-menu-btn ch-icon-container menu']").click
+
+    find(ta('EC:picker:content:pipelines')).click
+    # find(ta('EC:content:pipelines')).click
+    # find(:xpath, ta('EC:content:pipelines', "//a[@class='black-link nav-menu-category__item at-main-menu-pipelines']")).click
+    # find(:xpath, "//a[@class='black-link nav-menu-category__item at-main-menu-pipelines']").click
+
+    sleep 3
+
+    find(ta('EC:picker:choosePipeline')).click
+    # find(ta('EC:content:choosePipeline')).click
+    # find(:xpath, ta('EC:content:choosePipeline', "//div/a[@title='pipJul']")).click
+    # find(:xpath, "//div/a[@title='pipJul']").click
+
+    sleep 3
+
+    find(ta('EC:picker:content:newRunBtn')).click
+    # find(ta('EC:content:newRunBtn')).click
+    # find(:xpath, ta('EC:content:newRunBtn', "//div[@class='ec-icon-run-menu__square']")).click
+    # find(:xpath, "//div[@class='ec-icon-run-menu__square']").click
+
+    sleep 1
+
+    find(ta('EC:picker:content:newRun')).click
+    # find(ta('EC:content:newRun')).click
+    # find(:xpath, ta('EC:content:newRun', "(//div[@class='ec-icon-run-menu__col ec-icon-run-menu__col--label'])[1]")).click
+    # find(:xpath, "(//div[@class='ec-icon-run-menu__col ec-icon-run-menu__col--label'])[1]").click
+
+    find(ta('EC:picker:content:runBtn')).click
+    # find(ta('EC:content:runBtn')).click
+    # find(:xpath, ta('EC:content:runBtn', "//div[@class='at-pipeline-release-run-btn ec-wizard-btn ec-ok at-ok-btn']")).click
+    # find(:xpath, "//div[@class='at-pipeline-release-run-btn ec-wizard-btn ec-ok at-ok-btn']").click
+
+    sleep 5
+
+    find(ta('EC:picker:content:acceptStage')).click
+    # find(ta('EC:content:acceptStage')).click
+    # find(:xpath, ta('EC:content:acceptStage', "//span[@class='sds-svg-icon__wrapper at-approver-drop-down sds-svg-icon__wrapper--icon-menu-dropdown']")).click
+    # find(:xpath, "//span[@class='sds-svg-icon__wrapper at-approver-drop-down sds-svg-icon__wrapper--icon-menu-dropdown']").click
+
+    sleep 2
+
+    find(ta('EC:picker:content:manualTaskPopup')).click
+    # find(ta('EC:content:manualTaskPopup')).click
+    # find(:xpath, ta('EC:content:manualTaskPopup', "(//div[@class='run-app-manual-approvals__approvers'])[1]")).click
+    # find(:xpath, "(//div[@class='run-app-manual-approvals__approvers'])[2]").click
+
+    sleep 3
+
+    find(ta('EC:picker:content:completed')).click
+    # find(ta('EC:content:completed')).click
+    # find(:xpath, ta('EC:content:completed', "(//div[@class='response-dialog__outcome--item'])[2]")).click
+    # find(:xpath, "(//div[@class='response-dialog__outcome--item'])[2]").click
+
+    find(ta('EC:picker:content:okBtn')).click
+    # find(ta('EC:content:okBtn')).click
+    # find(:xpath, ta('EC:content:okBtn', "//div[@class='ec-wizard-btn ec-ok at-ok-btn']")).click
+    # find(:xpath, "//div[@class='ec-wizard-btn ec-ok at-ok-btn']").click
+
+    sleep 5
+
+    find(ta('EC:picker:content:acceptStage')).click
+    # find(ta('EC:content:acceptStage')).click
+    # find(:xpath, ta('EC:content:acceptStage', "//span[@class='sds-svg-icon__wrapper at-approver-drop-down sds-svg-icon__wrapper--icon-menu-dropdown']")).click
+    # find(:xpath, "//span[@class='sds-svg-icon__wrapper at-approver-drop-down sds-svg-icon__wrapper--icon-menu-dropdown']").click
+
+    sleep 3
+
+    # find(ta('EC:picker:content:manualTaskPopup')).click
+    # # find(ta('EC:content:manualTaskPopup')).click
+    # find(:xpath, ta('EC:content:manualTaskPopup', "(//div[@class='run-app-manual-approvals__approvers'])[1]")).click
+    # # find(:xpath, "(//div[@class='run-app-manual-approvals__approvers'])[2]").click
+
+    find(ta('EC:picker:content:approveRadioBtn')).click
+    # find(ta('EC:content:approveRadioBtn')).click
+    # find(:xpath, ta('EC:content:approveRadioBtn', "(//span[@class='react-form__radio-container__label__text'])[2]")).click
+    # find(:xpath, "(//span[@class='react-form__radio-container__label__text'])[2]").click
+
+    find(ta('EC:picker:content:okBtn')).click
+    # find(ta('EC:content:okBtn')).click
+    # find(:xpath, ta('EC:content:okBtn', "//div[@class='ec-wizard-btn ec-ok at-ok-btn']")).click
+    # find(:xpath, "//div[@class='ec-wizard-btn ec-ok at-ok-btn']").click
+
+  end
+end
